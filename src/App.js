2f18233314f934a97ddf3c8043b23b0440843386
Frequency <meowharry@mail.ru>
2020-10-04 10:56:30 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Masonry from "react-masonry-css";
 
+import ImgurCard from "./components/ImgurCard";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://api.imgur.com/3/gallery/hot/viral/0.json", {
+      headers: {
+        Authorization: "Client-ID 372efbdc08ad473",
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.data);
+          console.log(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  const breakpointColumnsObj = {
+    default: 3,
+    1060: 2,
+    500: 1,
+  };
+
+  if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Загрузка...</div>;
+  } else {
+    return (
+      <div>
+        <div className="welcome-header">
+          <h1>Imgur Gallery</h1>
+        </div>
+        <div
+          style={{
+            flexWrap: "wrap",
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {items.map((item, index) => (
+              <ImgurCard item={item} key={index} />
+            ))}
+          </Masonry>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ImgurCard.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faEye } from "@fortawesome/free-regular-svg-icons";
+import { Card } from "react-bootstrap";
+
+const ImgurCard = ({ item }) => {
+  const [like, setLike] = useState(false);
+
+  let handleLike = () => {
+    setLike(!like);
+    alert(like ? "unlike" : "like");
+  };
+
+  let sadCat404 =
+    "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F026%2F489%2Fcrying.jpg";
+
+  let makeImgurMedia = (item) => {
+    if (item.mp4) {
+      return (
+        <video className={"card-img-top"} src={item.mp4} autoPlay muted loop />
+      );
+    }
+    if (item.images) {
+      return item.images[0].link.endsWith("mp4") ? (
+        <video
+          className={"card-img-top"}
+          src={item.images[0].link}
+          autoPlay
+          muted
+          loop
+        ></video>
+      ) : (
+        <Card.Img variant="top" src={item.images[0].link} />
+      );
+    } else if (item.link.endsWith("jpg") || item.link.endsWith("png")) {
+      return <Card.Img variant="top" src={item.link} />;
+    } else {
+      return <Card.Img variant="top" src={sadCat404} />;
+    }
+  };
+
+  return (
+    <Card style={{ width: "18rem", padding: "" }}>
+      <Card.Title style={{ padding: "0.5rem" }}>{item.title}</Card.Title>
+      {makeImgurMedia(item)}
+      <Card.Body className={"custom-body"}>
+        <p onClick={(e) => handleLike()}>
+          <FontAwesomeIcon icon={like ? faHeartSolid : faHeart} />
+          {like ? item.ups + 1 : item.ups}
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faEye} /> {item.views}
+        </p>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ImgurCard;
